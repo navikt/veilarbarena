e87394eee2626fadfbd90f2d02d9667e754c5ec1
Oscar Eckbo <oscar.eckbo@nav.no>
2018-08-16 15:27:13 +0200

    [FO-1307] Bruker common-kode for serialisering

--- src/main/java/no/nav/fo/veilarbarena/service/OppfolgingsbrukerEndringTemplate.java
+++ src/main/java/no/nav/fo/veilarbarena/service/OppfolgingsbrukerEndringTemplate.java
@@ -1,8 +1,5 @@
 package no.nav.fo.veilarbarena.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.fo.veilarbarena.domain.Bruker;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,14 +7,12 @@ import org.springframework.kafka.core.KafkaTemplate;
 import javax.inject.Inject;
 
 import static no.nav.fo.veilarbarena.config.KafkaConfig.OPPFOLGINGSBRUKER_MED_ENDRING_SIDEN;
+import static no.nav.json.JsonUtils.toJson;
 
 
 @Slf4j
 public class OppfolgingsbrukerEndringTemplate {
     private KafkaTemplate<String, String> kafkaTemplate;
-    private ObjectMapper brukerMapper = new ObjectMapper()
-            .registerModule(new JavaTimeModule());
-
 
     @Inject
     public OppfolgingsbrukerEndringTemplate(KafkaTemplate<String, String> kafkaTemplate) {
@@ -25,18 +20,12 @@ public class OppfolgingsbrukerEndringTemplate {
     }
 
     void send(Bruker bruker) {
-        final String mappedBruker;
-        try {
-            mappedBruker = brukerMapper.writeValueAsString(bruker);
-        } catch (JsonProcessingException e) {
-            log.error("Kunne ikke serialisere Bruker", e);
-            return;
-        }
+        final String serialisertBruker = toJson(bruker);
 
         kafkaTemplate.send(
                 OPPFOLGINGSBRUKER_MED_ENDRING_SIDEN,
                 bruker.getAktoerid(),
-                mappedBruker
+                serialisertBruker
         );
         log.info("Bruker: {} har endringer, legger på kø", bruker.getAktoerid());
     }
--- src/test/java/no/nav/fo/veilarbarena/service/OppfolgingsbrukerEndringTemplateTest.java
+++ src/test/java/no/nav/fo/veilarbarena/service/OppfolgingsbrukerEndringTemplateTest.java
@@ -1,27 +1,23 @@
 package no.nav.fo.veilarbarena.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import no.nav.fo.veilarbarena.domain.Bruker;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Objects;
 
+import static no.nav.json.JsonUtils.fromJson;
+import static no.nav.json.JsonUtils.toJson;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OppfolgingsbrukerEndringTemplateTest {
 
     @Test
-    void serialiseringOgDeserialiseringAvBruker() throws IOException {
-        ObjectMapper mapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule());
+    void serialiseringOgDeserialiseringAvBruker() {
         final ZonedDateTime tidspunkt = new Timestamp(100000000000L).toLocalDateTime().atZone(ZoneId.systemDefault());
-
         Bruker b = new Bruker(
                 1L,
                 "test",
@@ -42,10 +38,10 @@ class OppfolgingsbrukerEndringTemplateTest {
                 tidspunkt
         );
 
-        final String serialisertBruker = mapper.writeValueAsString(b);
+        final String serialisertBruker = toJson(b);
         ConsumerRecord<String, String> cr = new ConsumerRecord<>("testTopic", 1, 1, "testKey", serialisertBruker);
 
-        Bruker deserialisertBruker = mapper.readValue(cr.value(), Bruker.class);
+        Bruker deserialisertBruker = fromJson(cr.value(), Bruker.class);
         assertThat(Objects.equals(deserialisertBruker, serialisertBruker));
     }
 
