b5267056ff97f4b76214ed8fe6b0eb1707d308bc
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-08-16 07:56:37 +0200

    [KAIZEN-0] Flere sikkerhetsproperties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,21 @@ import no.nav.fo.veilarbarena.config.ApplicationConfig;
 
 import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_NAME;
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
-import static no.nav.fo.veilarbarena.config.ApplicationConfig.AKTOER_V2_ENDPOINTURL;
-import static no.nav.fo.veilarbarena.config.ApplicationConfig.REDIRECT_URL_PROPERTY;
+import static no.nav.fo.veilarbarena.config.ApplicationConfig.*;
+import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_PASSWORD;
+import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_USERNAME;
+import static no.nav.sbl.dialogarena.common.abac.pep.service.AbacServiceConfig.ABAC_ENDPOINT_URL_PROPERTY_NAME;
+import static no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants.STS_URL_KEY;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
     public static void main(String... args) {
+        System.setProperty(SYSTEMUSER_USERNAME, getRequiredProperty("SRVVEILARBARENA_USERNAME"));
+        System.setProperty(SYSTEMUSER_PASSWORD, getRequiredProperty("SRVVEILARBARENA_PASSWORD"));
+        System.setProperty(STS_URL_KEY, getRequiredProperty(SECURITYTOKENSERVICE_URL));
         System.setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty(AKTOER_V2_ENDPOINTURL));
         System.setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty(REDIRECT_URL_PROPERTY));
+        System.setProperty(ABAC_ENDPOINT_URL_PROPERTY_NAME, getRequiredProperty(ABAC_PDP_ENDPOINT_URL));
 
         ApiApp.runApp(ApplicationConfig.class, args);
     }
