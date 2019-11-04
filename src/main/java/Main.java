3467f832a110b44d4808fa020d0163e8bcac1016
matiasvinjevoll <matias.vinjevoll@nav.no>
2019-11-04 16:15:06 +0100

    KAIZEN-0 flyttet NaisUtils til common

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import no.nav.apiapp.ApiApp;
 import no.nav.fo.veilarbarena.utils.MigrationUtils;
 import no.nav.fo.veilarbarena.config.ApplicationConfig;
 import no.nav.fo.veilarbarena.config.DbConfig;
-import no.nav.fo.veilarbarena.utils.NaiseratorUtils;
+import no.nav.common.utils.NaisUtils;
 import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 
 import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_NAME;
@@ -10,8 +10,8 @@ import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarbarena.config.ApplicationConfig.*;
 import static no.nav.fo.veilarbarena.config.DbConfig.VEILARBARENADB_PASSWORD;
 import static no.nav.fo.veilarbarena.config.DbConfig.VEILARBARENADB_USERNAME;
-import static no.nav.fo.veilarbarena.utils.NaiseratorUtils.getCredentials;
-import static no.nav.fo.veilarbarena.utils.NaiseratorUtils.getDefaultSecretPath;
+import static no.nav.common.utils.NaisUtils.getCredentials;
+import static no.nav.common.utils.NaisUtils.getDefaultSecretPath;
 import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_PASSWORD;
 import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_USERNAME;
 import static no.nav.sbl.dialogarena.common.abac.pep.service.AbacServiceConfig.ABAC_ENDPOINT_URL_PROPERTY_NAME;
@@ -21,7 +21,7 @@ public class Main {
     public static void main(String... args) {
         readFromConfigMap();
 
-        NaiseratorUtils.Credentials serviceUser = getCredentials(getDefaultSecretPath("service_user"));
+        NaisUtils.Credentials serviceUser = getCredentials(getDefaultSecretPath("service_user"));
         System.setProperty(SYSTEMUSER_USERNAME, serviceUser.username);
         System.setProperty(SYSTEMUSER_PASSWORD, serviceUser.password);
         System.setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
@@ -31,7 +31,7 @@ public class Main {
         System.setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty(REDIRECT_URL_PROPERTY));
         System.setProperty(ABAC_ENDPOINT_URL_PROPERTY_NAME, getRequiredProperty(ABAC_PDP_ENDPOINT_URL));
 
-        NaiseratorUtils.Credentials oracleCreds = getCredentials(getDefaultSecretPath("oracle_creds"));
+        NaisUtils.Credentials oracleCreds = getCredentials(getDefaultSecretPath("oracle_creds"));
         System.setProperty(VEILARBARENADB_USERNAME, oracleCreds.username);
         System.setProperty(VEILARBARENADB_PASSWORD, oracleCreds.password);
 
@@ -41,7 +41,7 @@ public class Main {
     }
 
     private static void readFromConfigMap() {
-        NaiseratorUtils.addConfigMapToEnv("pto-config",
+        NaisUtils.addConfigMapToEnv("pto-config",
                 "APPDYNAMICS_AGENT_ACCOUNT_NAME",
                 "APPDYNAMICS_CONTROLLER_HOST_NAME",
                 "APPDYNAMICS_CONTROLLER_PORT",
