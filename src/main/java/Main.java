8ca3396e4d06bcfa6989fae74b4a31f0366213ff
matiasvinjevoll <matias.vinjevoll@nav.no>
2019-11-07 12:27:45 +0100

    KAIZEN-0 sett systembruker for OIDC

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
 import no.nav.apiapp.ApiApp;
-import no.nav.fo.veilarbarena.utils.MigrationUtils;
+import no.nav.brukerdialog.tools.SecurityConstants;
+import no.nav.common.utils.NaisUtils;
 import no.nav.fo.veilarbarena.config.ApplicationConfig;
 import no.nav.fo.veilarbarena.config.DbConfig;
-import no.nav.common.utils.NaisUtils;
+import no.nav.fo.veilarbarena.utils.MigrationUtils;
+import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
 import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 
 import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_NAME;
+import static no.nav.common.utils.NaisUtils.getCredentials;
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarbarena.config.ApplicationConfig.*;
 import static no.nav.fo.veilarbarena.config.DbConfig.VEILARBARENADB_PASSWORD;
 import static no.nav.fo.veilarbarena.config.DbConfig.VEILARBARENADB_USERNAME;
-import static no.nav.common.utils.NaisUtils.getCredentials;
-import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_PASSWORD;
-import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_USERNAME;
 import static no.nav.sbl.dialogarena.common.abac.pep.service.AbacServiceConfig.ABAC_ENDPOINT_URL_PROPERTY_NAME;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
@@ -21,10 +21,19 @@ public class Main {
         readFromConfigMap();
 
         NaisUtils.Credentials serviceUser = getCredentials("service_user");
-        System.setProperty(SYSTEMUSER_USERNAME, serviceUser.username);
-        System.setProperty(SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //ABAC
+        System.setProperty(CredentialConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(CredentialConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //CXF
         System.setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
         System.setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //OIDC
+        System.setProperty(SecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
         System.setProperty(StsSecurityConstants.STS_URL_KEY, getRequiredProperty(SECURITYTOKENSERVICE_URL));
         System.setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty(AKTOER_V2_ENDPOINTURL));
         System.setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty(REDIRECT_URL_PROPERTY));
