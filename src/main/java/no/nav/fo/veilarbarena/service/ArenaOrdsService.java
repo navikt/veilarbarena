a46c054b6c40adebc7cbd4ac4660045dcc9d9a26
matiasvinjevoll <matias.vinjevoll@nav.no>
2019-11-28 15:40:22 +0100

    FO-2243 helsesjekk mot arena ords

--- src/main/java/no/nav/fo/veilarbarena/service/ArenaOrdsService.java
+++ src/main/java/no/nav/fo/veilarbarena/service/ArenaOrdsService.java
@@ -1,6 +1,8 @@
 package no.nav.fo.veilarbarena.service;
 
 import lombok.SneakyThrows;
+import no.nav.apiapp.selftest.Helsesjekk;
+import no.nav.apiapp.selftest.HelsesjekkMetadata;
 import no.nav.fo.veilarbarena.utils.ArenaOrdsTokenProvider;
 import no.nav.fo.veilarbarena.utils.ArenaOrdsUrl;
 import no.nav.sbl.rest.RestUtils;
@@ -10,7 +12,7 @@ import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 
-public class ArenaOrdsService {
+public class ArenaOrdsService implements Helsesjekk {
 
     private final ArenaOrdsTokenProvider arenaOrdsTokenProvider;
     private final Client client = RestUtils.createClient();
@@ -30,5 +32,22 @@ public class ArenaOrdsService {
                 .get();
 
         return response.readEntity(clazz);
+    }
+
+    @Override
+    public void helsesjekk() {
+        int status = client
+                .target(ArenaOrdsUrl.get("arena/api/v1/test/ping"))
+                .request()
+                .get()
+                .getStatus();
+        if (status != 200) {
+            throw new IllegalStateException("Feil i selftest mot Arena ORDS - status = " + status);
+        }
+    }
+
+    @Override
+    public HelsesjekkMetadata getMetadata() {
+        return new HelsesjekkMetadata("arena_ords_ping", ArenaOrdsUrl.get("arena/api/v1/test/ping"), "Arena ORDS - ping", false);
     }
 }
