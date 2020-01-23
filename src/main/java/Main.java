b61e03f11d3ec8dea790e5c6ad1764f2ad6678dd
Peder Korsveien <peder.korsveien@nav.no>
2020-01-23 14:58:10 +0100

    KZ-0 Les inn env fra configmap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import no.nav.apiapp.ApiApp;
 import no.nav.brukerdialog.tools.SecurityConstants;
-import no.nav.common.utils.NaisUtils;
+import no.nav.common.nais.utils.NaisUtils;
 import no.nav.fo.veilarbarena.config.ApplicationConfig;
 import no.nav.fo.veilarbarena.config.DbConfig;
 import no.nav.fo.veilarbarena.utils.MigrationUtils;
@@ -8,7 +8,7 @@ import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
 import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 
 import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_NAME;
-import static no.nav.common.utils.NaisUtils.getCredentials;
+import static no.nav.common.nais.utils.NaisUtils.getCredentials;
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarbarena.config.ApplicationConfig.*;
 import static no.nav.fo.veilarbarena.config.DbConfig.VEILARBARENADB_PASSWORD;
@@ -18,7 +18,6 @@ import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
     public static void main(String... args) {
-        readFromConfigMap();
 
         NaisUtils.Credentials serviceUser = getCredentials("service_user");
 
@@ -47,30 +46,5 @@ public class Main {
         MigrationUtils.createTables(DbConfig.getDataSource());
 
         ApiApp.runApp(ApplicationConfig.class, args);
-    }
-
-    private static void readFromConfigMap() {
-        NaisUtils.addConfigMapToEnv("pto-config",
-                "KAFKA_BROKERS_URL",
-                "SECURITYTOKENSERVICE_URL",
-                "ABAC_PDP_ENDPOINT_URL",
-                "ABAC_PDP_ENDPOINT_DESCRIPTION",
-                "ISSO_HOST_URL",
-                "ISSO_JWKS_URL",
-                "ISSO_ISSUER_URL",
-                "ISSO_ISALIVE_URL",
-                "VEILARBLOGIN_REDIRECT_URL_DESCRIPTION",
-                "VEILARBLOGIN_REDIRECT_URL_URL",
-                "AKTOER_V2_SECURITYTOKEN",
-                "AKTOER_V2_ENDPOINTURL",
-                "AKTOER_V2_WSDLURL",
-                "VIRKSOMHET_OPPFOELGINGSSTATUS_V2_SECURITYTOKEN",
-                "VIRKSOMHET_OPPFOELGINGSSTATUS_V2_ENDPOINTURL",
-                "VIRKSOMHET_OPPFOELGINGSSTATUS_V2_WSDLURL",
-                "LOGINSERVICE_OIDC_CALLBACKURI",
-                "LOGINSERVICE_OIDC_DISCOVERYURI",
-                "UNLEASH_API_URL",
-                "AKTOERREGISTER_API_V1_URL"
-        );
     }
 }
