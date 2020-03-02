395b807eafac1e9b61d1795d5ce1a5e4d8591fde
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-03-02 13:51:18 +0100

    FO-2599 bruk PepClient og aktør id i tilgangssjekk

--- src/main/java/no/nav/fo/veilarbarena/config/PepConfig.java
+++ src/main/java/no/nav/fo/veilarbarena/config/PepConfig.java
@@ -1,36 +1,22 @@
 package no.nav.fo.veilarbarena.config;
 
-import no.nav.apiapp.security.veilarbabac.VeilarbAbacPepClient;
-import no.nav.brukerdialog.security.oidc.SystemUserTokenProvider;
+import no.nav.apiapp.security.PepClient;
 import no.nav.sbl.dialogarena.common.abac.pep.Pep;
 import no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext;
-import no.nav.sbl.featuretoggle.unleash.UnleashService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import javax.inject.Inject;
+import static no.nav.sbl.dialogarena.common.abac.pep.domain.ResourceType.Person;
 
 @Configuration
 @Import({AbacContext.class, UnleashConfig.class})
 public class PepConfig {
 
-    private SystemUserTokenProvider systemUserTokenProvider = new SystemUserTokenProvider();
-
-    @Inject
-    private UnleashService unleashService;
+    public static final String DOMAIN_VEILARB = "veilarb";
 
     @Bean
-    public VeilarbAbacPepClient pepClient(Pep pep) {
-        VeilarbAbacPepClient.Builder builder = VeilarbAbacPepClient.ny()
-                .medPep(pep)
-                .medResourceTypePerson()
-                .medSystemUserTokenProvider(() -> systemUserTokenProvider.getToken())
-                .brukAktoerId(() -> unleashService.isEnabled("veilarboppfolging.veilarbabac.aktor"))
-                .sammenlikneTilgang(() -> unleashService.isEnabled("veilarboppfolging.veilarbabac.sammenlikn"))
-                .foretrekkVeilarbAbacResultat(() -> unleashService.isEnabled("veilarboppfolging.veilarbabac.foretrekk_veilarbabac"));
-
-        return builder.bygg();
+    public PepClient pepClient(Pep pep) {
+        return new PepClient(pep, DOMAIN_VEILARB, Person);
     }
-
 }
--- src/main/java/no/nav/fo/veilarbarena/service/AuthService.java
+++ src/main/java/no/nav/fo/veilarbarena/service/AuthService.java
@@ -1,7 +1,6 @@
 package no.nav.fo.veilarbarena.service;
 
-import no.nav.apiapp.security.veilarbabac.Bruker;
-import no.nav.apiapp.security.veilarbabac.VeilarbAbacPepClient;
+import no.nav.apiapp.security.PepClient;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -10,18 +9,17 @@ import javax.inject.Inject;
 public class AuthService {
 
     private final AktoerRegisterService aktoerRegisterService;
-    private final VeilarbAbacPepClient pepClient;
+    private final PepClient pepClient;
 
     @Inject
-    public AuthService(AktoerRegisterService aktoerRegisterService, VeilarbAbacPepClient pepClient) {
+    public AuthService(AktoerRegisterService aktoerRegisterService, PepClient pepClient) {
         this.aktoerRegisterService = aktoerRegisterService;
         this.pepClient = pepClient;
     }
 
     public void sjekkTilgang(String fnr) {
-            String aktorId = getAktorIdOrThrow(fnr);
-            Bruker bruker = Bruker.fraAktoerId(aktorId).medFoedselsnummer(fnr);
-            pepClient.sjekkLesetilgangTilBruker(bruker);
+        String aktorId = getAktorIdOrThrow(fnr);
+        pepClient.sjekkLesetilgangTilAktorId(aktorId);
     }
 
     private String getAktorIdOrThrow(String fnr) {
