18757099639b5113812ae4944e4b922b7a74b354
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-06-05 11:15:13 +0200

    Fiks serialisering av oppfolgingssakId

--- src/main/java/no/nav/veilarbarena/domain/api/OppfolgingssakDTO.java
+++ src/main/java/no/nav/veilarbarena/domain/api/OppfolgingssakDTO.java
@@ -3,6 +3,6 @@ package no.nav.veilarbarena.domain.api;
 import com.google.gson.annotations.SerializedName;
 
 public class OppfolgingssakDTO {
-    @SerializedName("saksId")
+    @SerializedName(value="oppfolgingssakId", alternate={"saksId"})
     String oppfolgingssakId;
 }
