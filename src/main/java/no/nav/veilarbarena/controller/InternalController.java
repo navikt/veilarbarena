5d9eba876415332d6fec7f096ff226fe0e47c40c
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-09-21 09:49:18 +0200

    Ta bort ORDS health check for /isReady

--- src/main/java/no/nav/veilarbarena/controller/InternalController.java
+++ src/main/java/no/nav/veilarbarena/controller/InternalController.java
@@ -1,12 +1,16 @@
 package no.nav.veilarbarena.controller;
 
-import no.nav.common.health.HealthCheck;
-import no.nav.common.health.HealthCheckUtils;
-import no.nav.common.health.selftest.*;
+import no.nav.common.health.HealthCheckResult;
+import no.nav.common.health.selftest.SelfTestChecks;
+import no.nav.common.health.selftest.SelfTestUtils;
+import no.nav.common.health.selftest.SelftTestCheckResult;
+import no.nav.common.health.selftest.SelftestHtmlGenerator;
+import no.nav.veilarbarena.utils.DatabaseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
+import static no.nav.common.health.HealthCheckUtils.safeCheckHealth;
 import static no.nav.common.health.selftest.SelfTestUtils.checkAllParallel;
 
 @RestController
@@ -23,23 +27,21 @@ public class InternalController {
 
     private final SelfTestChecks selftestChecks;
 
+    private final JdbcTemplate db;
+
     @Autowired
-    public InternalController(SelfTestChecks selftestChecks) {
+    public InternalController(SelfTestChecks selftestChecks, JdbcTemplate db) {
         this.selftestChecks = selftestChecks;
+        this.db = db;
     }
 
     @GetMapping("/isReady")
     public void isReady() {
-        List<HealthCheck> healthChecks =
-                selftestChecks.getSelfTestChecks().stream()
-                        .filter(SelfTestCheck::isCritical)
-                        .map(SelfTestCheck::getCheck)
-                        .collect(toList());
+        HealthCheckResult dbHealthCheckResult = safeCheckHealth(() -> DatabaseUtils.checkDbHealth(db));
 
-        HealthCheckUtils.findFirstFailingCheck(healthChecks)
-                .ifPresent((failedCheck) -> {
-                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-                });
+        if (dbHealthCheckResult.isUnhealthy()) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/isAlive")
