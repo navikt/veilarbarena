d123f4690889f1cdcba4d2415bfe4acac3f72bff
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-05-16 10:16:36 +0200

    Fix parsing av floats fra arena

--- src/main/java/no/nav/veilarbarena/utils/XmlUtils.java
+++ src/main/java/no/nav/veilarbarena/utils/XmlUtils.java
@@ -1,15 +1,20 @@
 package no.nav.veilarbarena.utils;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
 
+import java.io.IOException;
+
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 public class XmlUtils {
 
-    private final static XmlMapper xmlMapper;
+    private static final XmlMapper xmlMapper;
 
     static {
         JacksonXmlModule module = new JacksonXmlModule();
@@ -25,4 +30,17 @@ public class XmlUtils {
         return xmlMapper.readValue(xml, clazz);
     }
 
+    public static class ArenaFloatDeserializer extends JsonDeserializer<Float> {
+
+        @Override
+        public Float deserialize(JsonParser parser, DeserializationContext context)
+                throws IOException {
+            String floatString = parser.getText();
+            if (floatString.contains(",")) {
+                floatString = floatString.replace(",", ".");
+            }
+            return Float.valueOf(floatString);
+        }
+
+    }
 }
