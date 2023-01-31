5843784353fb9af43e6532d1cbf6bc30d5d29a5e
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-01-31 15:20:24 +0100

    h2 ny versjon

--- src/test/resources/db-init.sql
+++ src/test/resources/db-init.sql
@@ -63,8 +63,8 @@ CREATE SEQUENCE KAFKA_PRODUCER_RECORD_ID_SEQ;
 CREATE TABLE KAFKA_PRODUCER_RECORD (
     ID                      NUMBER(18) NOT NULL,
     TOPIC                   VARCHAR(100) NOT NULL,
-    KEY                     BLOB,
-    VALUE                   BLOB,
+    "KEY"		    BLOB,
+    "VALUE"                 BLOB,
     HEADERS_JSON            CLOB,
     CREATED_AT              TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (ID)	
