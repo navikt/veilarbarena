8a1e7f9ff1c0da3755171e0b255708841d4afe89
JulieHillRoa <julie.hill.roa@nav.no>
2023-03-20 15:48:06 +0100

    Legge til poao-tilgang for tilgangssjekk

--- src/main/java/no/nav/veilarbarena/service/AuthService.java
+++ src/main/java/no/nav/veilarbarena/service/AuthService.java
@@ -1,19 +1,27 @@
 package no.nav.veilarbarena.service;
 
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.abac.Pep;
 import no.nav.common.abac.domain.request.ActionId;
 import no.nav.common.auth.context.AuthContextHolder;
-import no.nav.common.client.aktoroppslag.AktorOppslagClient;
-import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
+import no.nav.poao_tilgang.client.Decision;
+import no.nav.poao_tilgang.client.NavAnsattTilgangTilEksternBrukerPolicyInput;
+import no.nav.poao_tilgang.client.PoaoTilgangClient;
+import no.nav.poao_tilgang.client.TilgangType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.ofNullable;
 
 @Slf4j
 @Service
@@ -23,18 +31,33 @@ public class AuthService {
 
     private final Pep veilarbPep;
 
+    private final PoaoTilgangClient poaoTilgangClient;
+
+    private final UnleashService unleashService;
+
     @Autowired
-    public AuthService(AuthContextHolder authContextHolder,  Pep veilarbPep) {
+    public AuthService(AuthContextHolder authContextHolder,  Pep veilarbPep, PoaoTilgangClient poaoTilgangClient, UnleashService unleashService) {
         this.authContextHolder = authContextHolder;
         this.veilarbPep = veilarbPep;
+        this.poaoTilgangClient = poaoTilgangClient;
+        this.unleashService = unleashService;
     }
 
     public void sjekkTilgang(Fnr fnr) {
         String innloggetBrukerToken = authContextHolder.requireIdTokenString();
+        if (unleashService.skalBrukePoaoTilgang()) {
+			Decision desicion = poaoTilgangClient.evaluatePolicy(new NavAnsattTilgangTilEksternBrukerPolicyInput(
+					hentInnloggetVeilederUUID(), TilgangType.LESE, fnr.get()
+			)).getOrThrow();
+			if (desicion.isDeny()) {
+				throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+			}
+        } else {
+			if (!veilarbPep.harTilgangTilPerson(innloggetBrukerToken, ActionId.READ, fnr)) {
+				throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+			}
+		}
 
-        if (!veilarbPep.harTilgangTilPerson(innloggetBrukerToken, ActionId.READ, fnr)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
     }
 
     public boolean erSystembruker() {
@@ -52,4 +75,17 @@ public class AuthService {
         }
     }
 
+	public static Optional<String> getStringClaimOrEmpty(JWTClaimsSet claims, String claimName) {
+		try {
+			return ofNullable(claims.getStringClaim(claimName));
+		} catch (Exception e) {
+			return empty();
+		}
+	}
+	public UUID hentInnloggetVeilederUUID() {
+		return authContextHolder.getIdTokenClaims()
+				.flatMap(claims -> getStringClaimOrEmpty(claims, "oid"))
+				.map(UUID::fromString)
+				.orElseThrow (() -> new ResponseStatusException(HttpStatus.FORBIDDEN, "Fant ikke oid for innlogget veileder") );
+	}
 }
