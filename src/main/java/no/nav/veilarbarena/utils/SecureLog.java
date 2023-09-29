8b27bb1899fcd19af0adaf63286d52da65c82f38
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-09-29 14:18:26 +0200

    fjerner fnr i url

--- src/main/java/no/nav/veilarbarena/utils/SecureLog.java
+++
@@ -1,8 +0,0 @@
-package no.nav.veilarbarena.utils;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class SecureLog {
-    public static Logger securelog = LoggerFactory.getLogger("SecureLog");
-}
