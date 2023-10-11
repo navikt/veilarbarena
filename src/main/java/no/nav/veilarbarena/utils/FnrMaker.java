6ab8bee3bbe018de02f288066bf3b2c5766305a7
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-10-11 10:53:17 +0200

    fjerner fnr i url, legg til tester

--- src/main/java/no/nav/veilarbarena/utils/FnrMaker.java
+++ src/main/java/no/nav/veilarbarena/utils/FnrMaker.java
@@ -1,11 +1,9 @@
 package no.nav.veilarbarena.utils;
 
 import no.nav.common.types.identer.Fnr;
-import org.json.JSONObject;
 
 public class FnrMaker {
     public static Fnr hentFnr(String fnr) {
-        JSONObject request = new JSONObject(fnr);
-        return Fnr.of(request.getString(fnr));
+        return Fnr.of(fnr);
     }
 }
