67a2cdabdd89f688e53230d7389b3d2a8eb7a56e
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-04-25 15:02:39 +0200

    Gi poao-tilgang mulighet for å force hentStatus fra arena synkront, siden synken fra arena er for treg. (#351)

--- src/main/java/no/nav/veilarbarena/service/ArenaService.java
+++ src/main/java/no/nav/veilarbarena/service/ArenaService.java
@@ -42,19 +42,26 @@ public class ArenaService {
 
     /**
      * Sjekker først i lokal database om bruker ligger der, hvis ikke så går vi direkte mot Arena for å hente status.
-     * @param fnr fødselsnummer/dnr til bruker
+     *
+     * @param fnr       fødselsnummer/dnr til bruker
+     * @param forceSync
      * @return status fra lokal database eller direkte fra Arena
      */
-    public Optional<ArenaStatusDTO> hentArenaStatus(Fnr fnr) {
-        var maybeDbLinkArenaStatus = oppfolgingsbrukerRepository.hentOppfolgingsbruker(fnr.get())
-                .map(DtoMapper::mapTilArenaStatusDTO);
+    public Optional<ArenaStatusDTO> hentArenaStatus(Fnr fnr, boolean forceSync) {
+        if (forceSync) {
+            return hentArenaOppfolgingsstatus(fnr)
+                    .map(DtoMapper::mapTilArenaStatusDTO);
+        } else {
+            var maybeDbLinkArenaStatus = oppfolgingsbrukerRepository.hentOppfolgingsbruker(fnr.get())
+                    .map(DtoMapper::mapTilArenaStatusDTO);
 
-        if (maybeDbLinkArenaStatus.isPresent()) {
-            return maybeDbLinkArenaStatus;
+            if (maybeDbLinkArenaStatus.isPresent()) {
+                return maybeDbLinkArenaStatus;
+            }
+
+            return hentArenaOppfolgingsstatus(fnr)
+                    .map(DtoMapper::mapTilArenaStatusDTO);
         }
-
-        return hentArenaOppfolgingsstatus(fnr)
-                .map(DtoMapper::mapTilArenaStatusDTO);
     }
 
     public Boolean hentKanEnkeltReaktiveres(Fnr fnr) {
