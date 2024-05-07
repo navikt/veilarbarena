9bbfa5a6db754e39233bc353df766597e8e73e46
Mads Lee Giil <mads.lee.giil@nav.no>
2024-05-07 09:07:56 +0200

    Vent også før forsøke på nytt pga IOException

--- src/main/java/no/nav/veilarbarena/utils/RetryUtils.java
+++ src/main/java/no/nav/veilarbarena/utils/RetryUtils.java
@@ -11,20 +11,22 @@ import java.util.logging.Logger;
 public class RetryUtils {
 
     private static final Logger log = Logger.getLogger(RetryUtils.class.getName());
+    private final static int msToWaitBetweenAttempts = 700;
 
     @SneakyThrows
     public static Response requestWithRetry(OkHttpClient client, Request request) {
         try {
             var response = client.newCall(request).execute();
             if(!response.isSuccessful()) {
+                Thread.sleep(msToWaitBetweenAttempts);
                 log.info("Kall feilet, forsøker på nytt");
-                Thread.sleep(700);
                 return client.newCall(request).execute();
             } else {
                 log.info("Kall var vellykket første gang");
                 return response;
             }
         } catch (IOException e) {
+            Thread.sleep(msToWaitBetweenAttempts);
             log.info("Kall feilet pga IOException, prøver likevel på nytt");
             return client.newCall(request).execute();
         }
